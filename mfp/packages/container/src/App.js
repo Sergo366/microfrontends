b07f41a85b17c4474daea681ef6a918275188667
skozenkov <kozenkovs366@gmail.com>
2025-07-17 21:27:59 +0200

    module federation with REACT

---
+++ mfp/packages/container/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { mount } from 'marketing/MarketingApp';
+
+console.log(mount)
+
+const App = () => {
+    return (
+        <div>
+            Container App
+        </div>
+    );
+};
+
+export default App;
